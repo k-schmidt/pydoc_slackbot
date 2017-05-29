ea6a722255fc23bb4a5ffc6c789ebb3c2e2b543e
Kyle Schmidt <kyle.a.schmidt@gmail.com>
2017-05-28 20:37:21 -0400

    adding help regex parsing

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Pydoc Slackbot
 
-A bot that prints the Python documentation for a module/function.
+A bot that provides the Python documentation for a module/function.
 
 Author:
+
 Kyle Schmidt <kyle.a.schmidt@gmail.com>
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import inspect
+import re
 import time
 
 from slackclient import SlackClient
@@ -7,19 +8,33 @@ from config import BOT_NAME, BOT_TOKEN, AT_BOT
 
 
 def handle_command(command, channel, slack_client):
-    response = inspect.getdoc(command)
-    slack_client.api_call("chat.postMessage",
-                          channel=channel,
-                          text=response,
-                          as_user=True)
+    try:
+        response = inspect.getdoc(eval(command))
+        slack_client.api_call("chat.postMessage",
+                              channel=channel,
+                              text=response,
+                              as_user=True)
+    except NameError as e:
+        slack_client.api_call("chat.postMessage",
+                              channel=channel,
+                              text="Sorry, I couldn't handle that",
+                              as_user=True)
+
+def parse_helper(response):
+    try:
+        m = re.search('(?<=help\()\w+', response)
+        return m.group(0)
+    except AttributeError as e:
+        return "Sorry, I didn't catch that"
 
 def parse_slack_output(slack_rtm_output):
     in_text = slack_rtm_output[0].get("text") if slack_rtm_output else None
     channel = slack_rtm_output[0].get("channel") if slack_rtm_output else None
-    print(in_text, channel)
     if in_text and channel and AT_BOT in in_text:
         response = (in_text.split(AT_BOT)[1].strip().lower())
-        return (response, channel)
+        help_string = parse_helper(response)
+        print(help_string)
+        return (help_string, channel)
     return None, None
 
 
